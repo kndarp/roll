4532eed4ee5b145b081d0828763a450764c456da
Kndarp Patel <kndarppatel@gmail.com>
2018-10-26 21:15:40 +0100

    added roll and stop css

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
 import React, { Component } from 'react';
-import one from './assets/one.svg';
+import one from './assets/three.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      roll: "dice-roll"
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={one} className="dice" alt="logo" />
+          <img src={one} className={this.state.roll ? "dice-roll" : "dice-stop"}  alt="dice" />
           <p>
             <code>kndarp/roll</code> 
           </p>
         </header>
       </div>
     );
+  }
+  
+  componentDidMount() {
+    setInterval(() => {
+      let current = this.state.roll;
+      this.setState({
+        roll: !current
+      });
+    }, 3000);
   }
 }
 
